8523fd9b6f628abf9c035fd3247a37bb34ddb5ac
Santosh-kafle <kafle.dai13@gmail.com>
2025-12-22 12:13:28 +0545

    update url constant

--- system/Modules/Shared/Constant/UrlConstant.php
+++ system/Modules/Shared/Constant/UrlConstant.php
@@ -4,11 +4,11 @@ namespace Modules\Shared\Constant;
 
 class UrlConstant
 {
-    public const PAYMENT_SUCCESS_URL = 'http://localhost:3000/api/user/orders/success';
-    public const PAYMENT_FAILURE_URL = 'http://localhost:3000/api/user/orders/payment-fail';
+    public const PAYMENT_SUCCESS_URL = 'https://www.singingbowlvillagenepal.com/api/user/orders/success';
+    public const PAYMENT_FAILURE_URL = 'https://www.singingbowlvillagenepal.com/api/user/orders/payment-fail';
     public const ESEWA_ENQUIRY_URL = 'https://uat.esewa.com.np/api/epay/transaction/status';
-    public const CARD_SUCCESS_REDIRECT_URL = 'http://localhost:3000/orders/success/';
-    public const CARD_FAILURE_REDIRECT_URL = 'http://localhost:3000/orders/fail/';
+    public const CARD_SUCCESS_REDIRECT_URL = 'https://www.singingbowlvillagenepal.com/orders/success/';
+    public const CARD_FAILURE_REDIRECT_URL = 'https://www.singingbowlvillagenepal.com/orders/fail/';
     public const IMEPAY_BASE_URL = 'https://stg.imepay.com.np:7979/api/Web/';
     public const KHALTI_BASE_URL = 'https://a.khalti.com/api/v2/epayment/initiate/';
     public const KHALTI_LOOKUP_URL = 'https://a.khalti.com/api/v2/epayment/lookup/';
