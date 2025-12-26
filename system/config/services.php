b6c891f866fbfc3da3a5516612afc8cb65595e7c
AmritBhusal <bhusalamrit41@gmail.com>
2025-12-26 10:55:57 +0000

    config updated or gogle 0auth

--- system/config/services.php
+++ system/config/services.php
@@ -34,7 +34,7 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => 'https://uat.zolpastore.com/auth/google/callback',
+        'redirect' => env('GOOGLE_REDIRECT_URL', 'https://www.singingbowlvillagenepal.com/api/user/auth/google/callback'),
     ],
 
     // 'esewa' => [
